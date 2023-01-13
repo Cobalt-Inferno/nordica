cfe381461b02ef03bfe3bcafa1c635080e753003
cobaltinferno <nil>
2023-01-12 22:43:57 -0500

    Add refresh

--- src/api.py
+++ src/api.py
@@ -2,8 +2,9 @@ import requests
 class BazaarItem():
     def __init__(self, item) -> None:
         self.item = item
-        self.data = requests.get('https://api.hypixel.net/skyblock/bazaar').json() # This should be changed, as it will only update when initialized.
-        
+        self.data = requests.get('https://api.hypixel.net/skyblock/bazaar').json()
+    def refresh(self) -> None:
+        self.data = requests.get('https://api.hypixel.net/skyblock/bazaar').json() 
     def grab_price(self):
         return ((self.data['products'])[self.item])['quick_status']['buyPrice']
 
